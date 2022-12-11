a669b886985ee56b978e5209209e4c7f1d8334f6
Gabriela Jauhar <gbjauhar@gmail.com>
2022-12-11 15:16:47 -0300

    feat: create customers put

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -15,10 +15,22 @@ export async function create(req, res) {
 export async function findAll(req, res) {
     try {
         const customers = await connection.query('SELECT * FROM customers;')
-        res.send(customers)
+        res.send(customers.rows)
 
     } catch (err) {
         res.sendStatus(500).send(err.message)
     }
 
+}
+
+export async function update(req, res){
+    const {name, phone, cpf, birthday} = req.body
+    const {id} = req.params
+    try{
+        await connection.query('UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5', [name, phone, cpf, birthday, id])
+        res.sendStatus(200)
+    } catch (err){
+        res.sendStatus(500)
+        console.log(err)
+    }
 }
--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import {create, findAll} from "../controllers/customers.controllers.js"
+import {create, findAll, update} from "../controllers/customers.controllers.js"
 
 const router = Router()
 
 router.post("/customers", create)
 router.get("/customers", findAll)
+router.put("/customers/:id", update)
 
 export default router
